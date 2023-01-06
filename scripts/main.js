83f63ee387f0c77bfe9a9027c16eea844a5dc804
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2023-01-06 20:03:49 +0100

    add the rest of the website

--- scripts/main.js
+++ scripts/main.js
@@ -7,14 +7,3 @@ menuLinks.forEach((link) => {
         // Voer de actie uit die je wilt uitvoeren als de gebruiker op de link klikt
     });
 });
-
-
-
-
-//leeftijd dropdown 0-100 loop
-for (var i = 0; i <= 100; i++) {
-    var option = document.createElement("option");
-    option.text = i;
-    option.value = i;
-    ageSelect.add(option);
-}
